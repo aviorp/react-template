d0a6fca602c37282758cded778d372d48864ae19
Avior Peretz <avior.peretz@aquasec.com>
2023-04-29 05:11:46 +0300

    🎨 style(App.tsx): rename Dashboard component to App The component name was changed to App to better reflect the purpose of the component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import MainLayout from "./layouts/MainLayout";
 
-export default function Dashboard() {
+export default function App() {
   return (
     <>
       <CssBaseline />
